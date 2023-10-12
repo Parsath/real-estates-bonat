6d5c4b3714902f66a6437233023e1f398e7618a9
Bilel Taktak <bilel.taktak@esprit.tn>
2023-10-12 16:39:34 +0200

    chore: add enumerations for models

---
+++ src/common/constants/appartment-type.enum.ts
@@ -0,0 +1,8 @@
+export enum AppartmentTypeEnum {
+  HOUSE = 'House',
+  FLAT = 'Flat',
+  APPARTMENT = 'Appartment',
+  OFFICE = 'Office',
+  STORE = 'Store',
+  ROOM = 'Room',
+}
---
+++ src/common/constants/lease-type.enum.ts
@@ -0,0 +1,4 @@
+export enum LeaseTypeEnum {
+  OPEN = 'Open end date',
+  DEFINED = 'Defined end date',
+}
