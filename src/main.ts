5bc67e0e3c81ad5e3e1c62a020cd775ca3c80bf9
Bilel Taktak <bilel.taktak@esprit.tn>
2023-10-12 15:33:38 +0200

    chore: configure the bootstrap file main.ts so it includes helmet, the swagger module docs and the global filters and pipes we created earlier

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppConfigService } from './config/app/config.service';
+import { SwaggerModule } from '@nestjs/swagger';
+import { createDocument } from './doc/swagger';
+import helmet from 'helmet';
+import { ValidationFilter } from './common/exceptions/filter.validation';
+import { validationPipe } from './common/exceptions/error.validation';
+import { NotFoundExceptionFilter } from './common/exceptions/entity-no-found.exception';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const appConfig: AppConfigService = app.get(AppConfigService);
+
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  app.use(helmet());
+
+  /*_________EXCEPTION_FILTERS___________*/
+  app.useGlobalFilters(new ValidationFilter());
+  app.useGlobalFilters(new NotFoundExceptionFilter());
+  app.useGlobalPipes(validationPipe);
+  /*_________Swagger_Config___________*/
+  SwaggerModule.setup('doc', app, createDocument(app));
+
+  await app.listen(appConfig.port);
 }
 bootstrap();
